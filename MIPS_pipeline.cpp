c5b6986b7eeddfc512553c174036ad756ac498ed
jontrossbach <jontrossbach@gmail.com>
2020-11-08 22:57:53 -0500

    MIPS_pipeline.cpp

--- MIPS_pipeline.cpp
+++ MIPS_pipeline.cpp
@@ -31,6 +31,8 @@ struct EXStruct {
     bool        alu_op;     //1 for addu, lw, sw, 0 for subu 
     bool        wrt_enable;
     bool        nop;  
+
+    bool        isBranch;
 };
 
 struct MEMStruct {
@@ -43,7 +45,10 @@ struct MEMStruct {
     bool        rd_mem;
     bool        wrt_mem; 
     bool        wrt_enable;    
-    bool        nop;    
+    bool        nop;
+
+    bool        isBranch;    
+    bool        is_I_type;
 };
 
 struct WBStruct {
@@ -164,7 +169,7 @@ class DataMem
             else cout<<"Unable to open file";
                 dmem.close();          
         }
-		
+
         bitset<32> readDataMem(bitset<32> Address)
         {	
 			string datamem;
@@ -213,7 +218,7 @@ void printState(stateStruct state, int cycle)
         printstate<<"State after executing cycle:\t"<<cycle<<endl; 
         
         printstate<<"IF.PC:\t"<<state.IF.PC.to_ulong()<<endl;        
-        printstate<<"IF.nop:\t"<<state.IF.nop<<endl; 
+        printstate<<"IF.nop:\t"<<(bool)state.IF.nop<<endl; 
         
         printstate<<"ID.Instr:\t"<<state.ID.Instr<<endl; 
         printstate<<"ID.nop:\t"<<state.ID.nop<<endl;
@@ -294,19 +299,108 @@ int main() {
     int cycle = 0;
     stateStruct state, newState;    
 
+// Init State
+    //WBStruct
+    state.WB.Wrt_data = bitset<32> (0);
+    state.WB.Rs = bitset<5> (0);
+    state.WB.Rt = bitset<5> (0);
+    state.WB.Wrt_reg_addr = bitset<5> (0);
+    state.WB.wrt_enable = 0;
+    state.WB.nop = 1;
+    
+    //MemStruct 
+    state.MEM.is_I_type = 0;
+    state.MEM.isBranch = 0;
+    state.MEM.PC = bitset<32> (0);
+    state.MEM.ALUresult = bitset<32> (0);
+    state.MEM.Store_data = bitset<32> (0);
+    state.MEM.Rs = bitset<5> (0);
+    state.MEM.Rt = bitset<5> (0);
+    state.MEM.Wrt_reg_addr = bitset<5> (0);
+    state.MEM.wrt_enable = 0;
+    state.MEM.wrt_mem = 0;
+    state.MEM.rd_mem = 0;
+    state.MEM.nop = 1;
+
+    //EXstruct
+    state.EX.isBranch = 0;
+    state.EX.PC = bitset<32> (0);
+    state.EX.Read_data1 = bitset<32> (0);
+    state.EX.Read_data2 = bitset<32> (0);
+    state.EX.Imm = bitset<16> (0);
+    state.EX.Rs = bitset<5> (0);
+    state.EX.Rt = bitset<5> (0);
+    state.EX.Wrt_reg_addr = bitset<5> (0);
+    state.EX.wrt_enable = 0;
+    state.EX.alu_op = 1;
+    state.EX.is_I_type = 0;
+    state.EX.wrt_mem = 0;
+    state.EX.rd_mem = 0;
+    state.EX.nop = 1;    
+
     //IDStruct ID;
+    state.ID.PC = bitset<32> (0);
     state.ID.Instr = bitset<32> (0);
-    state.ID.nop = true;
+    state.ID.nop = 1;
 
     //IFStruct IF;
     state.IF.PC = bitset<32> (0);    
-    state.IF.nop = false;    
+    state.IF.nop = 0;    
+
+// Init newState
+    //WBStruct
+    newState.WB.Wrt_data = bitset<32> (0);
+    newState.WB.Rs = bitset<5> (0);
+    newState.WB.Rt = bitset<5> (0);
+    newState.WB.Wrt_reg_addr = bitset<5> (0);
+    newState.WB.wrt_enable = 0;
+    newState.WB.nop = 1;
+
+    //MemStruct
+    newState.MEM.is_I_type = 0;
+    newState.MEM.isBranch = 0;
+    newState.MEM.PC = bitset<32> (0);
+    newState.MEM.ALUresult = bitset<32> (0);
+    newState.MEM.Store_data = bitset<32> (0);
+    newState.MEM.Rs = bitset<5> (0);
+    newState.MEM.Rt = bitset<5> (0);
+    newState.MEM.Wrt_reg_addr = bitset<5> (0);
+    newState.MEM.wrt_enable = 0;
+    newState.MEM.wrt_mem = 0;
+    newState.MEM.rd_mem = 0;
+    newState.MEM.nop = 1;
+
+    //EXstruct
+    newState.EX.isBranch = 0;
+    newState.EX.PC = bitset<32> (0);
+    newState.EX.Read_data1 = bitset<32> (0);
+    newState.EX.Read_data2 = bitset<32> (0);
+    newState.EX.Imm = bitset<16> (0);
+    newState.EX.Rs = bitset<5> (0);
+    newState.EX.Rt = bitset<5> (0);
+    newState.EX.Wrt_reg_addr = bitset<5> (0);
+    newState.EX.alu_op = 1;
+    newState.EX.is_I_type = 0;
+    newState.EX.wrt_enable = 0;
+    newState.EX.wrt_mem = 0;
+    newState.EX.rd_mem = 0;
+    newState.EX.nop = 1;
+
+    //IDStruct ID;
+    newState.ID.PC = bitset<32> (0);
+    newState.ID.Instr = bitset<32> (0);
+    newState.ID.nop = 1;
+
+    //IFStruct IF;
+    newState.IF.PC = bitset<32> (0);
+    newState.IF.nop = 0;
 
 
     bitset<32> RD2 = bitset<32> (0); //used in EX
     string imm_adrStr, opcode;
     char instrType;
     bool isLoad, isStore, isBranch;
+    int endCount=6;
 
     while (1) {
 
@@ -319,52 +413,61 @@ int main() {
 	    //wbMux();
 	    if (state.WB.wrt_enable){
                 myRF.writeRF(state.WB.Wrt_reg_addr, state.WB.Wrt_data);
+
+                //Pretend forwarding mux		
+                if (state.WB.Wrt_reg_addr == state.EX.Rs) state.EX.Read_data1 = newState.WB.Wrt_data; //state.EX.nop = 1;}
+                if (state.WB.Wrt_reg_addr == state.EX.Rt) state.EX.Read_data2 = newState.WB.Wrt_data; //state.EX.nop = 1;}
 	    }
+
 	}
 
         /* --------------------- MEM stage --------------------- */
       
         if(state.MEM.nop == 0) {
+
+            //I think something is going wrong in here
             
             if (state.MEM.wrt_mem) {
             myDataMem.writeDataMem(state.MEM.ALUresult, state.MEM.Store_data);
 	    }
+	    
+	    
 	    if (state.MEM.rd_mem) {
 	    newState.WB.Wrt_data = myDataMem.readDataMem(state.MEM.ALUresult);
             } else newState.WB.Wrt_data = state.MEM.ALUresult;
-
-	    newState.MEM.nop == state.MEM.nop;
 
             newState.WB.Rs = state.MEM.Rs;
             newState.WB.Rt = state.MEM.Rt;
             newState.WB.Wrt_reg_addr = state.MEM.Wrt_reg_addr;
             newState.WB.wrt_enable = state.MEM.wrt_enable;
+
+            //Pretend forwarding mux
+            if (state.MEM.wrt_mem) { state.EX.Read_data1 = newState.WB.Wrt_data; state.ID.nop = 1; state.IF.nop = 1; }
+	    else if (state.MEM.Wrt_reg_addr == state.EX.Rs && !state.MEM.is_I_type && !state.MEM.wrt_mem && !state.MEM.isBranch) state.EX.Read_data1 = newState.WB.Wrt_data;
+            if (state.MEM.wrt_mem) { state.EX.Read_data1 = newState.WB.Wrt_data; state.ID.nop = 1; state.IF.nop = 1; }
+	    else if (state.MEM.Wrt_reg_addr == state.EX.Rt && !state.MEM.is_I_type && !state.MEM.wrt_mem && !state.MEM.isBranch) state.EX.Read_data2 = newState.WB.Wrt_data;
+
+
 	}	
+	newState.WB.nop = state.MEM.nop;
 
         /* --------------------- EX stage --------------------- */
      
         if(state.EX.nop == 0) {
 
             //I_TypeMux();
-            if (state.EX.is_I_type == 1 ) {
-                if (state.EX.Imm[0] == 0) {
-                    imm_adrStr = "0000000000000000" + state.EX.Imm.to_string();
-                } else if (state.EX.Imm[0] == 1) {
-                    imm_adrStr = "1111111111111111" + state.EX.Imm.to_string();
-                }
-                bitset<32> finalImm(imm_adrStr);
-		RD2 = finalImm;
+            if (state.EX.is_I_type == 1 ) {  }
+	    else {
             
-                newState.MEM.PC = bitset<32> (RD2.to_ulong() + state.EX.PC.to_ulong());
-
-	    } else {
-            
-                RD2= state.EX.Read_data1;
+                RD2= state.EX.Read_data2;
 	    }
 
 	    if (state.EX.wrt_mem == 0 || state.EX.rd_mem == 0) {
                 //ALU();
                 newState.MEM.ALUresult = myALU.ALUOperation(state.EX.alu_op, state.EX.Read_data1, RD2);
+
+		//Place holder so the Testbench1 diff will shut up
+                newState.MEM.Store_data = RD2;
 	    } else {
 		newState.MEM.ALUresult = state.EX.Read_data1;
 	    }
@@ -374,7 +477,6 @@ int main() {
         }
 
 
-        newState.MEM.nop == state.EX.nop;
 
         newState.MEM.Rs = state.EX.Rs;
         newState.MEM.Rt = state.EX.Rt;
@@ -383,9 +485,11 @@ int main() {
         newState.MEM.PC = state.EX.PC;
         newState.MEM.Wrt_reg_addr = state.EX.Wrt_reg_addr;
         newState.MEM.wrt_enable = state.EX.wrt_enable;
+	newState.MEM.isBranch = state.EX.isBranch;
+        newState.MEM.is_I_type = state.EX.is_I_type;
 
 	}
-
+        newState.MEM.nop = state.EX.nop;
         /* --------------------- ID stage --------------------- */
 
         if(state.ID.nop == 0) {
@@ -393,16 +497,19 @@ int main() {
             
 
             opcode = state.ID.Instr.to_string().substr(0,6);
-           
+            
+	    cout << opcode << endl;
+
             //Decoder();
-            if (opcode == "000000") instrType = 'R';
-            else instrType = 'I';
+            if (opcode == "000000") { instrType = 'R'; }
+	    else if (opcode == "111111") { instrType = 'C'; }
+            else { instrType = 'I'; }
     	
             isLoad = (opcode == "100011");
             isStore = (opcode == "101011");
             isBranch = (opcode == "000100");
-            newState.EX.wrt_enable = (isLoad || isStore);
-           	//isEq = ????????????????????????????????????????????????;
+            newState.EX.wrt_enable = (!isStore && !isBranch);
+           	//isEq = ???:Q?????????????????????????????????????????????;
     
             //First if statement is for debug purposes???
             if (isLoad == true || isStore == true || state.ID.Instr[1] == 0) { //Instr[1] == 1 means addu
@@ -428,7 +535,7 @@ int main() {
                 newState.EX.Rt[0] = state.ID.Instr[16];
                 newState.EX.Rt[1] = state.ID.Instr[17];
                 newState.EX.Rt[2] = state.ID.Instr[18];
-                newState.EX.Rt[3] = state.ID.Instr[18];
+                newState.EX.Rt[3] = state.ID.Instr[19];
                 newState.EX.Rt[4] = state.ID.Instr[20];
         
                 newState.EX.Wrt_reg_addr[0] = state.ID.Instr[11];
@@ -437,11 +544,15 @@ int main() {
                 newState.EX.Wrt_reg_addr[3] = state.ID.Instr[14];
                 newState.EX.Wrt_reg_addr[4] = state.ID.Instr[15];
 
-	
+		//Place holder so the Testbench1 diff will shut up
+		 for (int i=0; i<16; i++){
+                   newState.EX.Imm[i] = state.ID.Instr[i];
+		 }
+
                 newState.EX.Read_data1 = myRF.readRF(newState.EX.Rs);
                 newState.EX.Read_data2 = myRF.readRF(newState.EX.Rt);
 
-            } else if (instrType == 'I'){
+            } else if (instrType == 'I') {
 
                 newState.EX.is_I_type = true;
 
@@ -451,50 +562,81 @@ int main() {
                 newState.EX.Rs[3] = state.ID.Instr[24];
                 newState.EX.Rs[4] = state.ID.Instr[25];
 
+		//Rt and Wrt_reg_addr are same in this case
                 newState.EX.Rt[0] = state.ID.Instr[16];
                 newState.EX.Rt[1] = state.ID.Instr[17];
                 newState.EX.Rt[2] = state.ID.Instr[18];
-                newState.EX.Rt[3] = state.ID.Instr[18];
+                newState.EX.Rt[3] = state.ID.Instr[19];
                 newState.EX.Rt[4] = state.ID.Instr[20];
+                //Read previous comment
+                newState.EX.Wrt_reg_addr[0] = state.ID.Instr[16];
+                newState.EX.Wrt_reg_addr[1] = state.ID.Instr[17];
+                newState.EX.Wrt_reg_addr[2] = state.ID.Instr[18];
+                newState.EX.Wrt_reg_addr[3] = state.ID.Instr[19];
+                newState.EX.Wrt_reg_addr[4] = state.ID.Instr[20];
 
                 for (int i=0; i<16; i++){
                    newState.EX.Imm[i] = state.ID.Instr[i];
                 } 	
 	        
 		newState.EX.Read_data1 = myRF.readRF(newState.EX.Rs);
-		//newState.EX.Read_data2 = myRF.readRF(newState.EX.Rt); // unsure if needed
+		newState.EX.Read_data2 = myRF.readRF(newState.EX.Rt); // unsure if needed
             }
     
-            //TODO
+            //TODO something is either wrong here or in MEM
 	    newState.EX.rd_mem = isLoad; //?true?false?;
             newState.EX.wrt_mem = isStore; //?true?false?;
 
+	    newState.EX.isBranch = isBranch;
 
-            newState.EX.nop = state.ID.nop;
             newState.EX.PC = state.ID.PC;
+           
+	    
+	    if (instrType == 'I' && newState.EX.Read_data1 == newState.EX.Read_data2) {
+                if (newState.EX.Imm[15] == 0) {
+                    imm_adrStr = "0000000000000000" + newState.EX.Imm.to_string();
+                } else if (newState.EX.Imm[15] == 1) {
+                    imm_adrStr = "1111111111111111" + newState.EX.Imm.to_string();
+                }
+                bitset<32> finalImm(imm_adrStr);
+                RD2 = finalImm;
+                RD2 <<= 2;
+                //newState.MEM.PC = bitset<32> (RD2.to_long() + state.EX.PC.to_long());
+                if (isBranch == 1) {
+                    state.IF.PC = bitset<32> ((int32_t)RD2.to_ulong() + (int32_t)state.EX.PC.to_ulong());
+                    newState.IF.PC = state.IF.PC;
+                    state.ID.nop = 1;
+                    state.EX.nop = 1;
+                    state.MEM.nop = 1;
+                    state.WB.nop = 1;
+               }
 
+            }
 	     
 	}
-
+        newState.EX.nop = state.ID.nop;
 
         /* --------------------- IF stage --------------------- */
 
         //checkNop();
         //state.IF.InstrMemory();
-	if (state.IF.nop == 0) {
+       if (state.IF.nop == 0) {
 	
 		newState.ID.Instr = myInsMem.readInstr(state.IF.PC.to_ulong());
 		newState.ID.nop = state.IF.nop;
-	
+	        newState.IF.nop = state.IF.nop;
                //PC_Adder();
                newState.IF.PC = PC_Adder(state.IF.PC);
 
+               //Pass PC for later use TODO PC math
                newState.ID.PC = state.IF.PC;
 
-	}
-
-
-        cycle++;
+	       if (myInsMem.readInstr(state.IF.PC.to_ulong()).to_string() == "11111111111111111111111111111111") {
+		       newState.IF.nop = true;
+		       newState.ID.nop = true;
+                       newState.IF.PC = state.IF.PC;
+	       }
+       }
 
         if (state.IF.nop && state.ID.nop && state.EX.nop && state.MEM.nop && state.WB.nop)
             break;
@@ -502,7 +644,14 @@ int main() {
         printState(newState, cycle); //print states after executing cycle 0, cycle 1, cycle 2 ... 
        
         state = newState; /*The end of the cycle and updates the current state with the values calculated in this cycle */ 
-                	
+                
+        //This needs fixing TODO
+	if(myInsMem.readInstr(state.IF.PC.to_ulong()).to_string() == "11111111111111111111111111111111" && endCount == 6) endCount=5;
+	if(endCount < 6) endCount--;
+	if(endCount == 0) break;
+
+        cycle++;
+
     }
     
     myRF.outputRF(); // dump RF;	
